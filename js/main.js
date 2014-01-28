a9559006316be1511d5fd0818de2bbe56681a431
Ronnie Chong <ronnie@otakuness.com>
2014-01-28 22:16:17 +1100

    Part 5

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 var app = {
-
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+       $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
     findByName: function() {
-        console.log('findByName');
+         var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        var self = this;
+       this.store = new MemoryStore(function() {
+             self.renderHomeView();
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
